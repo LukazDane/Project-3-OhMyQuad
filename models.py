5751501b76bf46289d3a30fc7b4d9bd46d10ab24
trrstnn <edeclarin@gmail.com>
2019-03-18 17:04:43 -0700

    create workout model

--- models.py
+++ models.py
@@ -1,1 +1,17 @@
-from app import db, marshmallow
+import datetime
+#import everything from peewee because we might need it 
+from peewee import *
+
+
+DATABASE = SqliteDatabase('ohmyquad.db')
+
+class Workout(Model):
+  name = CharField()
+  timestamp = DateTimeField(default=datetime.datetime.now)
+  #relate the post to the sub model
+  user = ForeignKeyField(Sub, backref="workouts") 
+
+  class Meta:
+    database = DATABASE
+    order_by = ('-timestamp',)
+    
