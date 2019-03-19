2611e752e040416dc9a93da578e299b74e77518b
trrstnn <edeclarin@gmail.com>
2019-03-19 13:51:01 -0700

    initialized db

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-from flask import render_template, flash, redirect, url_for
-from forms import SubForm, PostForm, CommentForm
-#This import makes our connection to the models
+from flask import Flask, g
 import models
+
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
 app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
+ 
+
+  # Handle requests when the come in (before) and when they complete (after)
+@app.before_request
+def before_request():
+    """Connect to the DB before each request."""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request():
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
 
 
+  # The root route will revert back to a simpler version that just returns some text
+@app.route('/')
+def index():
+    return 'Integrating models with this route'
 
-
+  # ...
+    
+    
+    
+    
 
 
 
