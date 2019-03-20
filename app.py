e82cac012be4a36d92146fc7b5639622ff8e10bc
trrstnn <edeclarin@gmail.com>
2019-03-19 21:28:07 -0700

    testing user model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, g
-import models
 
+from flask import Flask, g
+from flask import render_template, flash, redirect, url_for
+import models
+from forms import WorkoutForm
 
 DEBUG = True
 PORT = 8000
@@ -17,18 +19,29 @@ def before_request():
     g.db.connect()
 
 @app.after_request
-def after_request():
+def after_request(response):
     """Close the database connection after each request."""
     g.db.close()
     return response
 
 
   # The root route will revert back to a simpler version that just returns some text
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Integrating models with this route'
+  # a form variable representing the SubForm
+  form = WorkoutForm()
+  #check if the form submission is valid
+  if form.validate_on_submit():
+  #if it is create a new sub and redirect the user
+    models.Workout.create(
+        name=form.name.data.strip(), 
+        description=form.description.data.strip())
 
-  # ...
+  
+  
+  #if it isn't send them back to form
+  return render_template('new_workout.html', title="New Workout", form=form)
+
     
     
     
@@ -43,4 +56,10 @@ def index():
 
 if __name__ == '__main__':
   models.initialize()
+  try:
+     models.User.create_user(
+
+     )
+  except ValueError:
+      pass
   app.run(debug=DEBUG, port=PORT)
